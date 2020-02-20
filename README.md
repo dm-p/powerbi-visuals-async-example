b5c4bc7f8ab23dc25714a3f75262825c93622a62
dm-p <daniel@coacervo.co>
2020-02-21 09:38:48 +1300

    Simple commit to illustrate functioning async/await

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# powerbi-async
+# powerbi-visuals-async-example
+
 Simple Example of Using async/await in Custom Visual After Loading regenerator-runtime. The visual is otherwise boilerplate.
+
+Requested in Power BI Community forums [here](https://community.powerbi.com/t5/Custom-Visuals-Development/Can-t-get-async-wait-to-work-in-any-form-or-shape-within-a/m-p/936229) and [here](https://community.powerbi.com/t5/Custom-Visuals-Development/Custom-Visual-await-Promise/m-p/630433).
+
+Requires [`powerbi-visuals-tools`](https://github.com/microsoft/PowerBI-visuals-tools) > 3.1 installing globally.
+
+Everything else should work after running `npm i` and then `pbiviz start` from the root folder.
---
+++ src/visual.ts
@@ -0,0 +1,88 @@
+/*
+*  Power BI Visual CLI
+*
+*  Copyright (c) Microsoft Corporation
+*  All rights reserved.
+*  MIT License
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the ""Software""), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in
+*  all copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*  THE SOFTWARE.
+*/
+"use strict";
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "./../style/visual.less";
+import powerbi from "powerbi-visuals-api";
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import VisualObjectInstance = powerbi.VisualObjectInstance;
+import DataView = powerbi.DataView;
+import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
+
+import { VisualSettings } from "./settings";
+export class Visual implements IVisual {
+    private settings: VisualSettings;
+
+    constructor(options: VisualConstructorOptions) {
+        console.log('Visual constructor', options);
+    }
+
+    public update(options: VisualUpdateOptions) {
+        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
+        console.log('Visual update', options);
+        console.log('Start!');
+
+        function getPosts() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    console.log("Set Timeout");
+                    resolve(1);
+                }, 20);
+            });
+            } 
+        async function init() { 
+            console.log("Async function was started");
+            await getPosts().then((result) => { console.log("Promise result received") });
+        }
+        
+       (async () => {
+            try {
+                var text = await init();
+                console.log("Some code after async call!");
+            } catch (e) {}
+        })();
+
+        console.log('End!');
+    }
+
+    private static parseSettings(dataView: DataView): VisualSettings {
+        return <VisualSettings>VisualSettings.parse(dataView);
+    }
+
+    /**
+     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
+     * objects and properties you want to expose to the users in the property pane.
+     *
+     */
+    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
+        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+    }
+}
